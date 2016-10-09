718f0e6873064e4d45000c03776b060a303849a9
chaiying89 <364070589@qq.com>
2016-10-10 02:14:45 +0800

    log to wx

--- src/app/core/money/money.service.ts
+++ src/app/core/money/money.service.ts
@@ -62,10 +62,12 @@ export class MoneyService {
   private _requestPay(payargs: IWxPayArgs) {
     return new Promise((resolve, reject) => {
       let onBridgeReady = () => {
+        console.log('onBridgeReady')
         WeixinJSBridge.invoke(
           'getBrandWCPayRequest',
           payargs,
           (res) => {
+            console.log(res)
             // 使用以上方式判断前端返回,微信团队郑重提示:res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
             if (res.err_msg === 'get_brand_wcpay_request：ok') {
               resolve();
