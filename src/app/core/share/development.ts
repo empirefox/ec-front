7f77055468d673a2b4bdba02fbcd493ffc22bdfa
chenqin <446341632@qq.com>
2016-11-10 21:41:19 +0800

    changes

--- src/app/core/share/development.ts
+++ src/app/core/share/development.ts
@@ -2,7 +2,7 @@ import { ModeArgs } from './config';
 
 export const modeArgs: ModeArgs = {
   publicOrigin: 'http://www.silu333.com',
-  apiOrigin: 'https://api.silu333.com',
+  apiOrigin: 'http://api.silu333.com:8080',
   wxCodeEndpoint: 'https://open.weixin.qq.com/connect/oauth2/authorize',
   cdnImgOrigin: 'https://head.silu333.com',
   qrLogoUrl: '',
